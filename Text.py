449495f37a5c89854eb0e9b31b7b52b3d4fd6841
Ian Johnson <ianjjohnson@icloud.com>
2016-01-16 13:40:42 -0600

    Not sure what changes

--- Text.py
+++ Text.py
@@ -10,11 +10,9 @@ def sendText(text):
 	
 	file = open('config.txt', 'r')
 
-	content = file.read().split('/n')
-
-	sender = content[0]
-	password = content[1]
-	recipient = content[2]
+	sender = file.readline()
+	password = file.readline()
+	recipient = file.readline()
 
 	#This is a commnent
 	servr = smtplib.SMTP("smtp.gmail.com:587")

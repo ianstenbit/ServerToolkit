c1ec2d4ca2f598eb4487ce98ed822376db7cd19e
Pranav Harathi <phrocks254@gmail.com>
2016-01-16 11:47:12 -0600

    Changes directory

--- SubProcessor.py
+++ SubProcessor.py
@@ -2,16 +2,22 @@ import subprocess
 import os
 
 class SubProcessor():
-
-	def __init__(self, cwd):
-		self.cwd = cwd
-
-	def run(self, command):
-		args = command.split(" ")
-		needShell = False
-		if (args[0] == "cd"):
-			command += "; dirs"
-			needShell = True
-		return subprocess.check_output(command, shell=needShell);
+    
+    def __init__(self, cwd):
+        self.cwd = cwd
+        
+    def run(self, command):
+        os.chdir(self.cwd)
+        args = command.split(" ")
+        needShell = False
+        dirChanged = False
+        if (args[0] == "cd"):
+            command += "; dirs -l"
+            needShell = True
+            dirChanged = True
+        output = subprocess.check_output(command, shell=needShell);
+        if (dirChanged):
+            self.cwd = output.strip()
+        return output
 
 

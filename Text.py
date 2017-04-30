384fa5ac385d60460779bcbbd44b48d9734431c5
Jacob Franklin <jackobefranklin@gmail.com>
2017-04-30 14:39:00 -0700

    PEP-8-ify: remove unused imports, tabs=>spaces, general cleanup

--- Text.py
+++ Text.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+
 
 def sendText(text, sender, password, recipient):
+    servr = smtplib.SMTP("smtp.gmail.com:587")
+    servr.starttls()
+    servr.login(sender, password)
 
-	#This is a commnent
-	servr = smtplib.SMTP("smtp.gmail.com:587")
-	servr.starttls()
-	servr.login(sender, password)
+    servr.sendmail(sender, recipient, text.replace('/n/n', '/n'))
 
-        
-        servr.sendmail(sender, recipient, text.replace('/n/n', '/n'))
+    servr.quit()
 
-	servr.quit()
-
-	print "Sent Text: " + text
-
+    print "Sent Text: " + text
